3b44f4448564e978175dff96f08d859868bc7789
HyunSongKwon <201701198@o.cnu.ac.kr>
2019-12-16 00:54:45 +0900

    test code

---
+++ ANTLR_Project/Testfiles/jp_many_error.c
@@ -0,0 +1,16 @@
+
+int add(int x, int y) {
+	int z ;
+	z = x+y;
+	if(z>f and k>h){
+	y=i/2;
+	}
+	return j;
+}
+
+void main () {
+	int t = 33;
+	int k[3];
+	k[0] = 5;
+	_print(add(1, k[0]));
+}
---
+++ ANTLR_Project/Testfiles/jp_normal2.c
@@ -0,0 +1,30 @@
+int f=1;
+int k[6];
+void main() {
+int a=1;
+int b=3;
+int c;
+    while(a>10) {
+        a = 10;
+    }
+
+    if( a>5)
+        if(b<6)
+        while(c == a) {
+            a=b+c;
+            if(c<10)
+                ++ f;
+        }
+
+}
+
+int f(int  a,int    b) {
+
+    if(a>b) {
+        if(b<a)
+    return -    1;
+    }
+
+    return a
+    % b;
+}
